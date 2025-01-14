23944be8dcd9a7a8b524e3120294a22b7edb3f80
himanshuchadha <himanshuchadha2020@gmail.com>
2025-01-14 23:44:09 +0530

    edit

--- #6_JS Conditional_expressions.js
+++ #6_JS Conditional_expressions.js
@@ -2,10 +2,15 @@ console.log("conditional statements");
 
 //let a = prompt("your age:"); //this will give error in node js and wot give error in chrome
 //a = Number.parseInt(a);
-let a = 19;
+let a = prompt("your age:");
 
-if (a > 18) {
-  console.log("good");
+if (a < 0) {
+  console.log("Invalid age");
+} else if (a < 9) {
+  console.log("you are a kid");
+} else if (a < 18 && a > 9) {
+  console.log("you are a teenager");
 } else {
-  console.log("not good");
+  console.log("you are a adult");
 }
+console.log("finished");
